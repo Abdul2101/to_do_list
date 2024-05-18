11d18347e4cc3253e6b5245aca06849ba0f76bd9
Abdulkhalyk <itisabdul21@gmail.com>
2024-05-19 00:52:31 +0300

    форма 3 раза повоторяется

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -29,4 +29,8 @@ urlpatterns = [
     #todoa
     path('current/', views.currenttodos, name='currenttodos'),
     path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+
 ]
